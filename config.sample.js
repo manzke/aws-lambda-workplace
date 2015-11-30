354d6a85e7d7128944b4567060726bfc603da8a4
Daniel Manzke <daniel.manzke@googlemail.com>
2015-11-30 08:08:50 +0100

    cleaned up config sample

--- config.sample.js
+++ config.sample.js
@@ -1,14 +1,15 @@
 'use strict';
 
 var config = module.exports = {
-  kinesis : {
-    region : '',
-    name: ''
-  },
-  
-  workplace : {
-      domain : '',
-      username : '',
-      password : ''
-  }
+    kinesis : {
+        region : 'eu-west-1',
+        name: 'name_of_your_kinesis_stream'
+    },
+
+    workplace : {
+        domain : '',
+        username : '',
+        password : '',
+        metrics : 'documents_total_storage_size,usercount,documentcount,spacecount,foldercount,sharecount,documents_links_count,documents_comments_count,documents_activities_count,logged_in_usercount'
+    }
 };
